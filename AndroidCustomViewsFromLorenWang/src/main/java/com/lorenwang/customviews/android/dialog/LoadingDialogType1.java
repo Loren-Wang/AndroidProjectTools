46cd61ca3cf0c74c0f23d3c34a69a15aadc2ae2e
lorenwang <lorenwangdevelop@gmail.com>
2018-11-16 15:57:02 +0800

    增加第一种加载中弹窗

---
+++ AndroidCustomViewsFromLorenWang/src/main/java/com/lorenwang/customviews/android/dialog/LoadingDialogType1.java
@@ -0,0 +1,28 @@
+package com.lorenwang.customviews.android.dialog;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+
+import com.lorenwang.customviews.android.R;
+
+/**
+ * 创建时间：2018-11-16 下午 15:10:8
+ * 创建人：王亮（Loren wang）
+ * 功能作用：加载中弹窗
+ * 思路：
+ * 方法：
+ * 注意：
+ * 修改人：
+ * 修改时间：
+ * 备注：
+ */
+public class LoadingDialogType1 extends BaseDialog {
+    public LoadingDialogType1(Context context) {
+        super(context,R.style.loading_dialog_type1);
+        view = LayoutInflater.from(context).inflate(R.layout.loading_dialog_type_1,null);
+        new Builder(context,R.style.loading_dialog_type1).create();
+        setView(view);
+        setCanceledOnTouchOutside(false);
+        getWindow().setWindowAnimations(R.style.dialog_anim_for_center);
+    }
+}
